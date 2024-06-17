33067728686339890c299dc0370ed7b65ca9e24a
Brendan Shumberger <bshumberger@vt.edu>
2024-06-17 19:30:43 -0400

    Print in hamitonian for GA testing.

--- apyib/hamiltonian.py
+++ apyib/hamiltonian.py
@@ -16,6 +16,9 @@ class Hamiltonian(object):
         # Set the basis set for the calculation.
         psi4.set_options({'basis': parameters['basis']})
 
+        print("This is the parameters['geom']."
+        print(parameters['geom'])
+
         # Define the molecule and basis set as properties of the Hamiltonian.
         self.molecule = psi4.geometry(parameters['geom'])
         self.basis_set = psi4.core.BasisSet.build(self.molecule)
