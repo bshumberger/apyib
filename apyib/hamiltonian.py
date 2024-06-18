048c0f57b0e8d5b9f05d214438c87465ced7ae5f
Brendan Shumberger <bshumberger@vt.edu>
2024-06-17 21:09:11 -0400

    Tests are passing on GA. This was a Psi4 error with loading numpy version 2.

--- apyib/hamiltonian.py
+++ apyib/hamiltonian.py
@@ -16,16 +16,8 @@ class Hamiltonian(object):
         # Set the basis set for the calculation.
         psi4.set_options({'basis': parameters['basis']})
 
-        print("This is the parameters['geom'].")
-        print(parameters['geom'])
-
         # Define the molecule and basis set as properties of the Hamiltonian.
         self.molecule = psi4.geometry(parameters['geom'])
-        #self.molecule = psi4.core.Molecule.from_string(parameters['geom'])
-
-        print("This is the molecule.")
-        print(self.molecule.geometry().to_array())
-
         self.basis_set = psi4.core.BasisSet.build(self.molecule)
 
         # Use the MintsHelper to get the AO integrals.
