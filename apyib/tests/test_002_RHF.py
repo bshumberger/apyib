fc08753d57a679e9a3ca9737fe7a51ed0d5c6c5f
Brendan Shumberger <bshumberger@vt.edu>
2024-12-27 12:56:10 -0500

    Added methyl oxirane test for RHF and printing for normal modes.

--- apyib/tests/test_002_RHF.py
+++ apyib/tests/test_002_RHF.py
@@ -97,3 +97,49 @@ def test_rhf_energy():
 
     assert(abs(apyib_E_tot - psi4_RHF) < 1e-11)
 
+def test_rhf_energy_methyloxirane_6_31G():
+    # Set parameters for the calculation.
+    S_methyloxirane_aug_cc_pVDZ = """
+    C  -0.406643263371948   0.103169698978202  -0.932904009322372
+    O   1.550888890337005  -1.412171718063139   0.260072952566956
+    C   1.810064371203058   1.327408273345544   0.226984115881429
+    H   3.344838669765579   2.027224623297944  -0.964879943170253
+    H   1.599930911340288   2.226009101570543   2.076678955772736
+    C  -2.946240158335548   0.082079933386530   0.348469914386564
+    H  -4.122138549897469   1.649720715533630  -0.352252535246005
+    H  -3.940556043431783  -1.702386417970233  -0.037902074947365
+    H  -2.712874671266827   0.277413155894713   2.403143387960141
+    H  -0.412850332112492  -0.076749763394509  -2.996238617375079
+
+    no_com
+    no_reorient
+    symmetry c1
+    units bohr
+    """
+
+    parameters = {'geom': S_methyloxirane_aug_cc_pVDZ,
+                  'basis': '6-31G',
+                  'method': 'RHF',
+                  'e_convergence': 1e-12,
+                  'd_convergence': 1e-12,
+                  'DIIS': True,
+                  'freeze_core': False,
+                  'F_el': [0.0, 0.0, 0.0],
+                  'F_mag': [0.0, 0.0, 0.0],
+                  'max_iterations': 120}
+
+    # Run Psi4.
+    p4_E_tot, p4_wfn = apyib.utils.run_psi4(parameters)
+    psi4.core.clean()
+
+    # Setting RHF reference value.
+    psi4_RHF = p4_E_tot
+
+    # Compute energy.
+    E_list, T_list, C, basis = apyib.energy.energy(parameters)
+    apyib_E_tot = E_list[0] + E_list[1] + E_list[2]
+    
+    # Print energies and energy difference between apyib code and Psi4.    
+    print("Energy Difference between Homemade RHF Code and Psi4: ", apyib_E_tot - psi4_RHF)
+
+    assert(abs(apyib_E_tot - psi4_RHF) < 1e-11)
